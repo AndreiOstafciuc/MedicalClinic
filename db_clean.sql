0ccbe658cf8eaa85ffb3a84b5c61a144f4cae30f
JumpMan <MariusBordeian@users.noreply.github.com>
2016-01-03 15:10:18 +0200

    Create db_clean.sql

---
+++ db_clean.sql
@@ -0,0 +1,10 @@
+    delete from RESULT;
+    delete from SCHEDULE;
+    delete from APPOINTMENT;
+    delete from PATIENT;
+    delete from DOCTOR;
+    delete from ADMIN;
+    delete from DEPARTMENT;
+    
+    delete from CREDENTIALS;
+    commit;

9a0139c6ab76bdcaf672cd333bfc315252077af2
Roxana Cosmanescu <roxana.cosmanescu@yahoo.com>
2016-01-03 17:58:44 +0200

    integrated basic UI

---
+++ MedicalClinic/UIViews/DoctorUI.xaml.cs
@@ -0,0 +1,44 @@
+﻿using DAO;
+using DBConnNamespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UIViews
+{
+    /// <summary>
+    /// Interaction logic for DoctorUI.xaml
+    /// </summary>
+    public partial class DoctorUI : GenericUI
+    {
+        
+        public DoctorUI():base()
+        {
+            InitializeComponent();
+            
+        }
+        public override UserControl getHeader()
+        {
+            return new GenericControls.Header();
+        }
+        public override UserControl getFooter()
+        {
+            return new GenericControls.Footer();
+        }
+        public override UserControl getMainContent()
+        {
+            return new GenericControls.MainContent();
+        }
+    }
+}
---
+++ MedicalClinic/UIViews/GenericUI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UIViews
+{
+    public abstract class GenericUI: Grid
+    {
+        public abstract UserControl getHeader();
+        public abstract UserControl getFooter();
+        public abstract UserControl getMainContent();
+        public GenericUI() : base()
+        {
+            this.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+            UserControl header = getHeader();
+            UserControl footer = getFooter();
+            UserControl content = getMainContent();
+
+            this.RowDefinitions.Add(new RowDefinition());
+            this.RowDefinitions.Add(new RowDefinition());
+            this.RowDefinitions.Add(new RowDefinition());
+            this.RowDefinitions[0].Height = new GridLength(50);
+            this.RowDefinitions[1].Height = new GridLength(1, GridUnitType.Star);
+            this.RowDefinitions[2].Height = new GridLength(30);
+            Grid.SetRow(header, 0);
+            Grid.SetColumn(header, 0);
+            Grid.SetRow(content, 1);
+            Grid.SetColumn(content, 0);
+            Grid.SetRow(footer, 2);
+            Grid.SetColumn(footer, 0);
+
+            this.Children.Add(header);
+            this.Children.Add(content);
+            this.Children.Add(footer);
+        }
+    }
+}

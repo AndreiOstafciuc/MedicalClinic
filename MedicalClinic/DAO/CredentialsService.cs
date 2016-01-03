806103e89221948cb81a79479dcc5467aaf72472
AndreiOstafciuc <andreiostafciuc@gmail.com>
2016-01-03 11:34:45 +0200

    Updated services

---
+++ MedicalClinic/DAO/CredentialsService.cs
@@ -0,0 +1,119 @@
+﻿using Entity;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class CredentialsService : DAO<Credentials>
+    {
+        public override List<Credentials> FindAllByProperty(string property, string value)
+        {
+            List<Credentials> credentialsList = null;
+
+            string sql = " select * from credentials where " + property + " like '" + value + "'";
+
+            _command.CommandText = sql;
+            _command.CommandType = CommandType.Text;
+            try
+            {
+                _dataReader = _command.ExecuteReader();
+
+
+                if (_dataReader.HasRows)
+                {
+                    credentialsList = new List<Credentials>();
+                    while (_dataReader.Read() && _dataReader.HasRows)
+                    {
+
+                        Credentials c = new Credentials();
+
+                        c.Id = Convert.ToInt32(_dataReader["id"]);
+                        c.Email = _dataReader["email"].ToString();
+                        c.Password = _dataReader["password"].ToString();
+                        c.Type = Convert.ToInt32(_dataReader["type"]);
+                        credentialsList.Add(c);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return credentialsList;
+        }
+
+        public override Credentials FindById(int id)
+        {
+            Credentials c = null;
+            string sql = " select * from credentials where id = " + id;
+
+            _command.CommandText = sql;
+            _command.CommandType = CommandType.Text;
+            try
+            {
+                _dataReader = _command.ExecuteReader();
+
+                _dataReader.Read();
+
+                if (_dataReader.HasRows)
+                {
+                    c = new Credentials();
+                    c.Id = Convert.ToInt32(_dataReader["id"]);
+                    c.Email = _dataReader["email"].ToString();
+                    c.Password = _dataReader["password"].ToString();
+                    c.Type = Convert.ToInt32(_dataReader["type"]);
+
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return c;
+        }
+
+        public override void Save(Credentials obj)
+        {
+            string sql = "insert into credentials values (" + obj.Id + ",'" + obj.Email + "','" + obj.Password + "','" + obj.Type + "')";
+
+            _command.CommandText = sql;
+            _command.CommandType = CommandType.Text;
+            try
+            {
+                _command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public override void Update(Credentials obj)
+        {
+
+            _command.CommandType = CommandType.Text;
+            _command.CommandText = "UPDATE credentials " +
+                              "SET email = :email, " +
+                              "password = :password, " +
+                              "type = :type " +
+                              "WHERE id = :id";
+            _command.Parameters.Add(":email", OracleDbType.Varchar2).Value = obj.Email;
+            _command.Parameters.Add(":password", OracleDbType.Varchar2).Value = obj.Password;
+            _command.Parameters.Add(":type", OracleDbType.Int32).Value = obj.Type;
+            _command.Parameters.Add(":id", OracleDbType.Int32).Value = obj.Id;
+            try
+            {
+                _command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ MedicalClinic/DAO/ResultsService.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DAO
+{
+    public class ResultsService : DAO<Results>
+    {
+        public override List<Results> FindAllByProperty(string property, string value)
+        {
+            List<Results> resultsList = null;
+
+            string sql = " select * from result where " + property + " like '" + value + "'";
+
+            _command.CommandText = sql;
+            _command.CommandType = CommandType.Text;
+            try
+            {
+                _dataReader = _command.ExecuteReader();
+
+
+                if (_dataReader.HasRows)
+                {
+                    resultsList = new List<Results>();
+                    while (_dataReader.Read() && _dataReader.HasRows)
+                    {
+
+                        Results r = new Results();
+
+                        r.Id = Convert.ToInt32(_dataReader["id_result"]);
+                        r.IdAppointment = Convert.ToInt32(_dataReader["id_appointment"]);
+                        r.ResultDate = Convert.ToDateTime(_dataReader["result_date"]);
+                        r.Symptoms = _dataReader["symptoms"].ToString();
+                        r.Diagnosis = _dataReader["diagnosis"].ToString();
+                        r.Medication = _dataReader["medication"].ToString();
+                        resultsList.Add(r);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return resultsList;
+        }
+
+        public override Results FindById(int id)
+        {
+            Results r = null;
+            string sql = " select * from result where id_result = " + id;
+
+            _command.CommandText = sql;
+            _command.CommandType = CommandType.Text;
+            try
+            {
+                _dataReader = _command.ExecuteReader();
+
+                _dataReader.Read();
+
+                if (_dataReader.HasRows)
+                {
+                    r = new Results();
+                    r.Id = Convert.ToInt32(_dataReader["id_result"]);
+                    r.IdAppointment = Convert.ToInt32(_dataReader["id_appointment"]);
+                    r.ResultDate = Convert.ToDateTime(_dataReader["result_date"]);
+                    r.Symptoms = _dataReader["symptoms"].ToString();
+                    r.Diagnosis = _dataReader["diagnosis"].ToString();
+                    r.Medication = _dataReader["medication"].ToString();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return r;
+        }
+
+        public override void Save(Results obj)
+        {
+            string sql = "insert into result values (" + obj.Id + ",'" + obj.IdAppointment + "','" + obj.ResultDate.ToString("dd-MMM-yy") + "','" + obj.Symptoms + "','" + obj.Diagnosis + "','" + obj.Medication + "')";
+
+            _command.CommandText = sql;
+            _command.CommandType = CommandType.Text;
+
+            try
+            {
+                _command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                // poate aparea contraint violation
+                // poate facem un logger ceva + un sistem de afisare a errorilor pentru utilizator
+            }
+        }
+
+        public override void Update(Results obj)
+        {
+            _command.CommandType = CommandType.Text;
+            _command.CommandText = "UPDATE result " +
+                              "SET id_appointment = :id_appointment, " +
+                              "result_date = :result_date, " +
+                              "symptoms = :symptoms, " +
+                              "diagnosis = :diagnosis, " +
+                              "medication = :medication " +
+                              "WHERE id_result = :id_result";
+            _command.Parameters.Add(":id_appointment", OracleDbType.Int32).Value = obj.IdAppointment;
+            _command.Parameters.Add(":result_date", OracleDbType.Date).Value = obj.ResultDate;
+            _command.Parameters.Add(":symptoms", OracleDbType.Varchar2).Value = obj.Symptoms;
+            _command.Parameters.Add(":diagnosis", OracleDbType.Varchar2).Value = obj.Diagnosis;
+            _command.Parameters.Add(":medication", OracleDbType.Varchar2).Value = obj.Medication;
+            _command.Parameters.Add(":id_result", OracleDbType.Int32).Value = obj.Id;
+            try
+            {
+                _command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}

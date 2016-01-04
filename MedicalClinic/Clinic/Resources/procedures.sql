d55f08602b9b486b707e00fa74b677c9cdd78551
AndreiOstafciuc <andreiostafciuc@gmail.com>
2016-01-04 23:45:58 +0200

    Added prcedures file

---
+++ MedicalClinic/Clinic/Resources/procedures.sql
@@ -0,0 +1,14 @@
+﻿create or replace procedure check_user
+(
+v_email IN CREDENTIALS.EMAIL%type,
+v_password IN CREDENTIALS.PASSWORD%type,
+v_id OUT CREDENTIALS.ID%type,
+v_type OUT CREDENTIALS.TYPE%type
+)
+is
+begin
+    select id,type into v_id,v_type from credentials where email like v_email and password like v_password;
+    exception  when no_data_found then
+        v_id:=0;
+        v_type:=0;
+end;

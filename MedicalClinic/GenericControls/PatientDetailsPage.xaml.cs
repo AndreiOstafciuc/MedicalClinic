4f67e4da0a8db710fd905afacf5ed1c600176d1a
Roxana Cosmanescu <roxana.cosmanescu@yahoo.com>
2016-01-06 22:49:11 +0200

    added images from resources to pageUserDetails, modified homePage buttons height

--- MedicalClinic/GenericControls/PatientDetailsPage.xaml.cs
+++ MedicalClinic/GenericControls/PatientDetailsPage.xaml.cs
@@ -37,11 +37,39 @@ namespace GenericControls
             Patient patient = _patientService.FindById(SessionData.UserSessionData.CurrentUserId);
             if (patient != null && credentials != null)
             {
+                setImages();
                labelPatientName.Content=patient.FirstName+" "+patient.LastName;
                labelPatientEmail.Content = credentials.Email;
                labelPatientPhone.Content = patient.PhoneNumber;
                labelPatientAddress.Content = patient.Address;
             }
+        }
+        private void setImages()
+        {
+            BitmapImage bmpUser = new BitmapImage();
+            bmpUser.BeginInit();
+            bmpUser.UriSource = new Uri("pack://application:,,,/Clinic;component/Resources/user.png");
+            bmpUser.EndInit();
+            imgUser.Source = bmpUser;
+
+            BitmapImage bmpPhone = new BitmapImage();
+            bmpPhone.BeginInit();
+            bmpPhone.UriSource= new Uri("pack://application:,,,/Clinic;component/Resources/phone.png");
+            bmpPhone.EndInit();
+            imgPhone.Source = bmpPhone;
+
+            BitmapImage bmpEmail = new BitmapImage();
+            bmpEmail.BeginInit();
+            bmpEmail.UriSource = new Uri("pack://application:,,,/Clinic;component/Resources/email.png");
+            bmpEmail.EndInit();
+            imgEmail.Source = bmpEmail;
+
+            BitmapImage bmpAddress = new BitmapImage();
+            bmpAddress.BeginInit();
+            bmpAddress.UriSource = new Uri("pack://application:,,,/Clinic;component/Resources/address.png");
+            bmpAddress.EndInit();
+            imgAddress.Source = bmpAddress;
+
         }
     }
 }

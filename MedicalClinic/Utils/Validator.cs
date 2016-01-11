0f2b50658c844b5d886905b285fb364c258dca7e
Roxana Cosmanescu <roxana.cosmanescu@yahoo.com>
2016-01-11 19:02:42 +0200

    added class for validating emails and phone numbers

---
+++ MedicalClinic/Utils/Validator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class Validator
+    {
+        public static bool ValidateEmail(String email)
+        {
+           return Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+        }
+        public static bool ValidatePhoneNumber(String phoneNumber)
+        {
+            return Regex.IsMatch(phoneNumber, "^[0-9]+$", RegexOptions.IgnoreCase);
+        }
+    }
+}
